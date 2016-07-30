d6d71a0103957ef749740d692e8aa5e54fb4d8e8
Lorenzo Iannuzzi <ivalue@privacy.com>
2016-07-30 14:22:17 +0200

    Added Nav Bar

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home | how2code.io</title>
+        <link href="css/master.css" rel="stylesheet">
+    </head>
+    <body>
+        <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#tut">Tutorials</a></li>
+            <li><a href="#proj">Projects</a></li>
+            <li><a href="#about">About Us</a></li>
+        </ul>
+    </body>
+</html>
